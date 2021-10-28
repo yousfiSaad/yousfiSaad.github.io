a3b46dab784ff9e5c088d7d7ab320424e5742c85
yousfi saad <yousfi.saad@gmail.com>
2021-10-28 12:08:18 +0200

    Add hover and provinces

--- morocco-map/provinces/index.js
+++ morocco-map/provinces/index.js
@@ -13,22 +13,22 @@ const svg = d3.select('svg').attr("width", width).attr("height", height);
 
 const text = svg.append("text").attr('y', 100).attr('x', 300).attr("text-anchor", "end");
 
-
-d3.json('https://cdn.jsdelivr.net/npm/morocco-map/data/regions.json')
+d3.json('https://cdn.jsdelivr.net/npm/morocco-map/data/provinces.json')
   .then(data => {
-    const regions = topojson.feature(data, data.objects.regions);
+    const provinces = topojson.feature(data, data.objects.provinces);
 
-    const projection = d3.geoMercator().fitSize([width, height], regions);
+    const projection = d3.geoMercator().fitSize([width, height], provinces);
     const pathGenerator = d3.geoPath().projection(projection);
 
-    svg.selectAll('path').data(regions.features)
+    svg.selectAll('path').data(provinces.features)
       .enter().append('path')
       .attr('class', 'region')
       .attr('d', pathGenerator)
       .on('mouseover', function (d, i) {
-        text.text(d.properties['name:ar'])
+        text.text(d.properties['name'])
       })
       .on('mouseout', function (d, i) {
         text.text("")
       });
+
   });
